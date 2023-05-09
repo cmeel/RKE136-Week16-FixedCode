cd3cb0f34ff05589945a7adf96e40107c4cbd40f
Carmel Meelind <cmeelind@tktk.ee>
2023-05-09 16:18:24 +0300

    add week16 task

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const axios = require('axios');
+const http = require('http');
+const mainRouter = require('./routes/mainRouters');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(mainRouter);
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running');
+});
